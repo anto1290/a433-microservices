f463058d6c948ed613cd37cd0579dfefeb1728e6
Muhammad Nurwibawanto <mnurwibawanto@gmail.com>
2023-11-29 18:42:51 +0700

    Membuat berkas shell script build_push_image_karsajobs.sh

---
+++ build_push_image_karsajobs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# membuat image karsajobs dan memiliki tag latest 
+docker build -t ghcr.io/anto1290/karsajobs:latest .
+# Login github
+echo $PASSWORD_GITHUB_PACKAGES | docker login ghcr.io -u anto1290 --password-stdin
+
+# Mempush image ke GitHub Packages
+docker push ghcr.io/anto1290/karsajobs:latest
