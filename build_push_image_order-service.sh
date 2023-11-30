56e7bc6124050b90c78244d21d128e59cd4ecdc0
Muhammad Nurwibawanto <mnurwibawanto@gmail.com>
2023-11-30 08:59:43 +0700

    Mengubah berkas .env dan membuat berkas shell script build_push_image_order-service.sh

---
+++ build_push_image_order-service.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#membuat image karsajobs dan memiliki tag latest 
+docker build -t ghcr.io/anto1290/order-service:latest .
+
+# Login github
+echo $PASSWORD_GITHUB_PACKAGES | docker login ghcr.io -u anto1290 --password-stdin
+
+# Mempush image ke GitHub Packages
+docker push ghcr.io/anto1290/order-service:latest
